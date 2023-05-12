8b044dc3fce1cac8053ca3a2bda25d344fb38ec6
Rhuan P <noreplay@example.com>
2023-05-12 12:26:09 -0300

    refactor(custom trie): useless code removed and optimized code

--- static/js/globalClass.js
+++ static/js/globalClass.js
@@ -8,42 +8,24 @@ class CustomTrie{
     }
     
     add(string,value){
-        let tail = this.T
         let T = this.T
-    
-        if(T[string[0]] === undefined){
-            T[string[0]] = {}
-            T.prev = T
-        }
-    
-        T = T[string[0]]
-    
 
-        for (let index = 1; index <= string.length; index++) {
+        for (let index = 0; index <= string.length; index++) {
 
             if(T[string[index]] === undefined && index < string.length ){
                 T[string[index]] = {}
             }
 
-            T.prev = tail
-
-            if(!tail.value){
-                tail.value = []
+            if(!T.value){
+                T.value = []
             }
     
-            tail.value[tail.value.length] = value
+            T.value[T.value.length] = value
     
             T = T[string[index]]
 
-            tail = tail[string[index-1]]
-
         }
 
-        if(!tail.value){
-            tail.value = []
-        }
-    
-        tail.value[tail.value.length] = value
     }
 
     search(string){
